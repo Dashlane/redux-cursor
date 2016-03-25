371c1cd551c1ee9f7568e2844e5deabea311deda
Denis Sokolov <denis.sokolov@dashlane.com>
2016-03-25 13:34:03 +0100

    Clarify rootCursor factory

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import cursor from './lib/cursor'
 import localReducer from './lib/localReducer'
-import rootCursor from './lib/rootCursor'
 import rootReducer from './lib/rootReducer'
 
 export const makeLocalReducer = localReducer
-export const makeRootCursor = rootCursor
 export const makeRootReducer = rootReducer
+
+// Naming makes it clear for the users that you are only supposed
+// to make a cursor at the root level, and then should use .child instead.
+// Contrast this with reducers, which have their own local reducer factory.
+export const makeRootCursor = cursor
 
 import { Action, Cursor, CursorAction, LocalReducer } from './lib/types'
 
