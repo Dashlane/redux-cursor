23b7780c5bd72593ca9e60f4d90a9390d469a2b0
Denis Sokolov <denis.sokolov@dashlane.com>
2016-03-25 08:57:16 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ import rootCursor from './rootCursor'
 export const makeLocalStateReducer = localReducer
 export const makeRootCursor = rootCursor
 export const makeRootReducer = cursorReducer
+
+import { Action, Cursor, CursorAction, LocalStateReducer, RootCursor } from './types'
+export type Action = Action
+export type CursorAction<Param> = CursorAction<Param>
+export interface Cursor<State, GlobalState> extends Cursor<State, GlobalState> {}
+export interface LocalStateReducer<State, GlobalState> extends LocalStateReducer<State, GlobalState> {}
+export interface RootCursor<GlobalState> extends RootCursor<GlobalState> {}
